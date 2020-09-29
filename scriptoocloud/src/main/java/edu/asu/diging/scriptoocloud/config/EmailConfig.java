df9dc0ed93082db5690d66aa0a989748c987eed4
jormsby2 <jormsby2@asu.edu>
2020-09-29 12:06:33 -0700

    [STC-2] Put all simple user configs in EmailConfig

--- scriptoocloud/src/main/java/edu/asu/diging/scriptoocloud/config/EmailConfig.java
+++ scriptoocloud/src/main/java/edu/asu/diging/scriptoocloud/config/EmailConfig.java
@@ -17,14 +17,15 @@ public class EmailConfig implements SimpleUsersConfiguration{
     
     @Override
     public void configure(SimpleUsers simpleUsers) {
-      simpleUsers.emailUsername(env.getRequiredProperty("email.user"));
-      simpleUsers.emailPassword(env.getRequiredProperty("email.password"));
-      simpleUsers.emailServerHost(env.getRequiredProperty("email.ServerHost"));
-      simpleUsers.emailServerPort(env.getRequiredProperty("email.ServerPort"));
-      simpleUsers.emailFrom(env.getRequiredProperty("email.emailFrom"));
-      simpleUsers.instanceUrl(env.getRequiredProperty("email.instanceUrl")); 
-      simpleUsers.emailStartTlsEnable(Boolean.parseBoolean(env.getRequiredProperty("email.tls")));
-      simpleUsers.appName(env.getRequiredProperty("email.appName"));
+      simpleUsers.emailUsername(env.getRequiredProperty("email.user"))
+      .emailPassword(env.getRequiredProperty("email.password"))
+      .emailServerHost(env.getRequiredProperty("email.ServerHost"))
+      .emailServerPort(env.getRequiredProperty("email.ServerPort"))
+      .emailFrom(env.getRequiredProperty("email.emailFrom"))
+      .instanceUrl(env.getRequiredProperty("email.instanceUrl"))
+      .emailStartTlsEnable(Boolean.parseBoolean(env.getRequiredProperty("email.tls")))
+      .appName(env.getRequiredProperty("email.appName"))
+      .resetRequestSentEndpoint("/reset/sent");
     }
 
 }
