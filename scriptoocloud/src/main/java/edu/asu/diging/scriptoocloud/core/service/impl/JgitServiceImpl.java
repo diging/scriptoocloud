8911a8499b4890a56a378eac9942f9e5b5cc1fa0
jormsby2 <jormsby2@asu.edu>
2020-11-01 19:26:28 -0700

    Revert "[STC-15] Updated jgit service to make tar of cloned repository"
    This reverts commit d37b32e37fd21dd15cdb245de2f91e61a584ec0b.

--- scriptoocloud/src/main/java/edu/asu/diging/scriptoocloud/core/service/impl/JgitServiceImpl.java
+++ scriptoocloud/src/main/java/edu/asu/diging/scriptoocloud/core/service/impl/JgitServiceImpl.java
@@ -1,15 +1,10 @@
 package edu.asu.diging.scriptoocloud.core.service.impl;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
-import org.eclipse.jgit.api.ArchiveCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.api.errors.JGitInternalException;
-import org.eclipse.jgit.archive.TarFormat;
-import org.eclipse.jgit.lib.Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,13 +24,6 @@ class JgitServiceImpl implements JgitService{
             Git.cloneRepository().setURI(url)
             .setDirectory(new File(filename))
             .call().getRepository().close();
-            
-            Git git = Git.open(new File(filename));
-            FileOutputStream out = new FileOutputStream("Docker.tar");
-            Repository db = git.getRepository();
-
-            git.archive().setTree(db.resolve("HEAD")).setOutputStream(out) .call();
-            
         }catch(GitAPIException e){
             deleteFilesService.deleteDirectoryContents(new File(filename));
             throw new InvalidGitUrlException(e);
@@ -43,11 +31,7 @@ class JgitServiceImpl implements JgitService{
         catch(JGitInternalException e){
             deleteFilesService.deleteDirectoryContents(new File(filename));
             throw new InvalidGitUrlException(e);
-        } catch (IOException e) {
-            deleteFilesService.deleteDirectoryContents(new File(filename));
-            throw new InvalidGitUrlException(e);
         }
-        
     }
 
 }
