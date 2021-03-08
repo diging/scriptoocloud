754c20eae2a2c412c586ef1e8eae694f2c3d0841
Joeseph Ferland <jferland@asu.edu>
2021-03-07 17:47:14 -0700

    [STC-10] Fixed some indentation

--- scriptoocloud/src/main/java/edu/asu/diging/scriptoocloud/core/service/impl/ProjectManagerImpl.java
+++ scriptoocloud/src/main/java/edu/asu/diging/scriptoocloud/core/service/impl/ProjectManagerImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.util.NestedServletException;
 
 import edu.asu.diging.scriptoocloud.core.data.ProjectRepository;
+import edu.asu.diging.scriptoocloud.core.exceptions.NullIDException;
 import edu.asu.diging.scriptoocloud.core.model.Project;
 import edu.asu.diging.scriptoocloud.core.model.impl.ProjectImpl;
 import edu.asu.diging.scriptoocloud.core.service.ProjectManager;
@@ -22,32 +23,37 @@ import edu.asu.diging.simpleusers.core.model.IUser;
 @Service
 public class ProjectManagerImpl implements ProjectManager {
 
-    @Autowired
-    private ProjectRepository projectRepo;
-    
-    /* (non-Javadoc)
-     * @see edu.asu.diging.scriptoocloud.core.service.impl.ProjectManager#createProject(java.lang.String, java.lang.String)
-     */
+	@Autowired
+	private ProjectRepository projectRepo;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * edu.asu.diging.scriptoocloud.core.service.impl.ProjectManager#createProject(
+	 * java.lang.String, java.lang.String)
+	 */
 	@Override
-    public Project createProject(String name, String description, IUser user) {
-        Project project = new ProjectImpl();
-        project.setName(name);
-        project.setUser(user);
-        project.setDescription(description);
-        project.setCreationDate(ZonedDateTime.now());
-        
-        return projectRepo.save((ProjectImpl)project);
-    }
-    
-    /* (non-Javadoc)
-     * 
-     */
+	public Project createProject(String name, String description, IUser user) {
+		Project project = new ProjectImpl();
+		project.setName(name);
+		project.setUser(user);
+		project.setDescription(description);
+		project.setCreationDate(ZonedDateTime.now());
+
+		return projectRepo.save((ProjectImpl) project);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 */
 	@Override
-    public int getProject(int id) {
-    	Optional<ProjectImpl> project = projectRepo.findById((long) id);
-        return id;
-    }
-    
+	public int getProject(int id) {
+		Optional<ProjectImpl> project = projectRepo.findById((long) id);
+		return id;
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -64,26 +70,27 @@ public class ProjectManagerImpl implements ProjectManager {
 		}
 	}
 
-/* (non-Javadoc)
-    * 
-    */
-   @Override
-    public List<Project> findAll() {
-        Iterable<ProjectImpl> projects = projectRepo.findAll();
-        List<Project> results = new ArrayList<>();
-        projects.forEach(e -> results.add((Project) e));
-        return results;
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 */
+	@Override
+	public List<Project> findAll() {
+		Iterable<ProjectImpl> projects = projectRepo.findAll();
+		List<Project> results = new ArrayList<>();
+		projects.forEach(e -> results.add((Project) e));
+		return results;
+	}
 
-@Override
-public void updateProject(int id,String name, String description) {
-	Project project = new ProjectImpl();
-    project.setId(id);
-	project.setName(name);
-    project.setDescription(description);
-    
-    //try
-	projectRepo.save((ProjectImpl)project);
-}
-    
+	@Override
+	public void updateProject(int id, String name, String description) {
+		Project project = new ProjectImpl();
+		project.setId(id);
+		project.setName(name);
+		project.setDescription(description);
+
+		// try
+		projectRepo.save((ProjectImpl) project);
+	}
+
 }
